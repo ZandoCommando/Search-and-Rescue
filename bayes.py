8e51fde7906cf4b0595be6001cf4335ad02d746d
ZandoCommando <xboyzander@gmail.com>
2022-10-12 19:17:35 -0700

    Bayes Fifth Commit: Legend Labels Added

--- bayes.py
+++ bayes.py
@@ -53,4 +53,8 @@ class Search:
         cv.rectangle(self.img, (SA3_CORNERS[0], SA3_CORNERS[1]), (SA3_CORNERS[2], SA3_CORNERS[3]), (0, 0, 0), 1)
         cv.putText(self.img, "1", (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
 
+        cv.putText(self.img, "+", (last_known), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        cv.putText(self.img, "+ = Last Known Position", (274, 355), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        cv.putText(self.img, "* = Actual Position", (275, 370), 1, (255, 0, 0))
+
 
