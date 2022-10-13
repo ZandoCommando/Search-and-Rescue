20205dffc9fb480db201175d07c1604641f9d54e
ZandoCommando <xboyzander@gmail.com>
2022-10-12 19:04:53 -0700

    Bayes Third Commit: Draw Map method instantiated and scale line created

--- bayes.py
+++ bayes.py
@@ -35,3 +35,10 @@ class Search:
         self.sep2 = 0
         self.sep3 = 0
 
+    def draw_map(self, last_known):
+        """ Displays map with scale, last known x/y coordinates and search areas """
+        cv.line(self.img, (20, 370), (70, 370), (0, 0, 0), 2) # Line for Map Scale
+        # Text Surrounding Line Scale
+        cv.putText(self.img, '0', (8, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        cv.putText(self.img, '50 Nautical Miles', (71, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+
