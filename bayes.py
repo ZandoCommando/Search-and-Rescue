941b9415d625adb00591ef8225b3d98ea4012e04
ZandoCommando <xboyzander@gmail.com>
2022-10-12 19:26:45 -0700

    Bayes Seventh Commit: Corrections and Updates

--- bayes.py
+++ bayes.py
@@ -23,9 +23,9 @@ class Search:
         self.area_actual = 0
         self.sailor_actual = [0, 0]
 
-        self.sa1 = self.img[SA1_CORNERS[1], SA1_CORNERS[3], SA1_CORNERS[2], SA1_CORNERS[4]]
-        self.sa2 = self.img[SA2_CORNERS[1], SA2_CORNERS[3], SA2_CORNERS[2], SA2_CORNERS[4]]
-        self.sa3 = self.img[SA3_CORNERS[1], SA3_CORNERS[3], SA3_CORNERS[2], SA3_CORNERS[4]]
+        self.sa1 = self.img[SA1_CORNERS[1]: SA1_CORNERS[3], SA1_CORNERS[0]: SA1_CORNERS[2]]
+        self.sa2 = self.img[SA2_CORNERS[1]: SA2_CORNERS[3], SA2_CORNERS[0]: SA2_CORNERS[2]]
+        self.sa3 = self.img[SA3_CORNERS[1]: SA3_CORNERS[3], SA3_CORNERS[0]: SA3_CORNERS[2]]
 
         self.p1 = 0.2
         self.p2 = 0.5
@@ -48,17 +48,17 @@ class Search:
         cv.putText(self.img, "1", (SA1_CORNERS[0] + 3, SA1_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
 
         cv.rectangle(self.img, (SA2_CORNERS[0], SA2_CORNERS[1]), (SA2_CORNERS[2], SA2_CORNERS[3]), (0, 0, 0), 1)
-        cv.putText(self.img, "1", (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.putText(self.img, "2", (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
 
         cv.rectangle(self.img, (SA3_CORNERS[0], SA3_CORNERS[1]), (SA3_CORNERS[2], SA3_CORNERS[3]), (0, 0, 0), 1)
-        cv.putText(self.img, "1", (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.putText(self.img, "2", (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
 
         cv.putText(self.img, "+", (last_known), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
         cv.putText(self.img, "+ = Last Known Position", (274, 355), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
-        cv.putText(self.img, "* = Actual Position", (275, 370), 1, (255, 0, 0))
+        cv.putText(self.img, "* = Actual Position", (275, 370), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
 
         cv.imshow("Search Area", self.img)
         cv.moveWindow("Search Area", 750, 10)
-        cv.waitKey(500)
+        cv.waitKey(10000)
 
 
